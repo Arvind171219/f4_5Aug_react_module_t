04b844d4a4ce847688ca8526e8b54c5eb9f64f43
Arvind kumar <arvindkr40882@gmail.com>
2023-08-05 22:41:27 +0530

    complate

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import Detail from './Detail';
+import React from "react";
+import "./styles.css";
+import Homepage from "./pages/Homepage.js";
+import {Routes, Route} from "react-router-dom";
+import Item from "./pages/item";
+import { BrowserRouter } from 'react-router-dom';
 
-const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/item/:id" component={Detail} />
-      </Switch>
-    </Router>
-  );
-};
-
-export default App;
+const App=()=>{
+    return(
+        <div>
+        <BrowserRouter>
+            <Routes>
+                <Route  path="/" element={<Homepage />} />
+                <Route  path="/item/:id" element={<Item />} />
+            </Routes>
+        </BrowserRouter>
+      </div>
+    )
+}
+export default App
